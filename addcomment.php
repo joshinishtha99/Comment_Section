c8b108be944d7530fc7b6b421b269a55384f7195
joshinishtha99 <111273916+joshinishtha99@users.noreply.github.com>
2024-09-19 11:30:34 +0530

    Update addcomment.php

--- addcomment.php
+++ addcomment.php
@@ -23,80 +23,78 @@
     $password = "";
     $database = "comment_section";
 
-    $conn = mysqli_connect($servername,$username,$password,$database); // establishing connection
+    $conn = mysqli_connect($servername, $username, $password, $database); // establishing connection
 
     if(!$conn){
-      die("Sorry we failed  to connect: " . mysqli_connect_error()); // exits program if executed
+      die("Sorry we failed to connect: " . mysqli_connect_error()); // exits program if executed
     }
-    else{   //Submit to a database
-    $sql = "INSERT INTO `comment_section` (`fullname`, `comment`) VALUES ('$name', '$comment_new');";
-    $result = mysqli_query($conn, $sql);
+    else{   // Submit to a database
+      $sql = "INSERT INTO `comment_section` (`fullname`, `comment`, `created_at`) VALUES ('$name', '$comment_new', NOW());";
+      $result = mysqli_query($conn, $sql);
 
-    if($result){
-      echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
-    <strong>Success!</strong> Your comment has been submitted.
-    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-  </div>';
-    }
-    else{
-      echo '<div class="alert alert-warning alert-dismissible fade show" role="alert">
-    <strong>Failed!</strong> Your comment has not been submitted due to server error.
-    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-  </div>';
-      echo mysqli_error($conn);
+      if($result){
+        echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+        <strong>Success!</strong> Your comment has been submitted.
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>';
+      }
+      else{
+        echo '<div class="alert alert-warning alert-dismissible fade show" role="alert">
+        <strong>Failed!</strong> Your comment has not been submitted due to server error.
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>';
+        echo mysqli_error($conn);
+      }
     }
   }
-}
-echo '<h1 align="center" class="mt-1">Comments</h1>';
-echo '<hr>';
 
- $servername = "localhost";
- $username = "root";
- $password = "";
- $database = "comment_section";
+  echo '<h1 align="center" class="mt-1">Comments</h1>';
+  echo '<hr>';
 
- $conn = mysqli_connect($servername,$username,$password,$database); // establishing connection
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $database = "comment_section";
 
- if(!$conn){
-   die("Sorry we failed  to connect: " . mysqli_connect_error()); // exits program if executed
- }
+  $conn = mysqli_connect($servername, $username, $password, $database); // establishing connection
 
- //selecting DATA
- $sql = "SELECT * FROM `comment_section`";
- $result = mysqli_query($conn, $sql);
+  if(!$conn){
+    die("Sorry we failed to connect: " . mysqli_connect_error()); // exits program if executed
+  }
 
- //find number of records returned
- $num = mysqli_num_rows($result);
+  // Selecting DATA
+  $sql = "SELECT * FROM `comment_section`";
+  $result = mysqli_query($conn, $sql);
 
- //Display the rows returned by SQL Query
- while($row = mysqli_fetch_assoc($result)){
-   echo '<div class="container mt-1">
-   <div class="card" align="center" style="width:81rem;">
- <div class="card-body" align="left">
-   <h5 class="card-title">'.$row['fullname'].'</h5>
-   <p class="card-text">'.$row['comment']. '</p>
- </div>
-</div>
-</div><br>';
- }
+  // Display the rows returned by SQL Query
+  while($row = mysqli_fetch_assoc($result)){
+    echo '<div class="container mt-1">
+    <div class="card" align="center" style="width:81rem;">
+      <div class="card-body" align="left">
+        <h5 class="card-title">'.$row['fullname'].' <small class="text-muted">'.$row['created_at'].'</small></h5>
+        <p class="card-text">'.$row['comment']. '</p>
+      </div>
+    </div>
+    </div><br>';
+  }
 
- echo '<hr>';
-  ?>
+  echo '<hr>';
+?>
 
   <!-- Form Layout, for adding name and comment -->
-  <div class = "container mt-3">
+  <div class="container mt-3">
     <h2>Add Comment</h2>
     <form action="/akshat/addcomment.php" method="post">
-  <div class="mb-3">
-    <label for="name" class="form-label">Name</label>
-    <input type="text" name="name" class="form-control" id="name">
-  </div>
-  <div class="mb-3">
-    <label for="comment" class="form-label">Comment</label>
-    <textarea type="comment" name="comment" class="form-control" id="comment" aria-describedby="emailHelp"></textarea>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+      <div class="mb-3">
+        <label for="name" class="form-label">Name</label>
+        <input type="text" name="name" class="form-control" id="name" required>
+      </div>
+      <div class="mb-3">
+        <label for="comment" class="form-label">Comment</label>
+        <textarea name="comment" class="form-control" id="comment" aria-describedby="emailHelp" required></textarea>
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
   </div>
   <br>
 
